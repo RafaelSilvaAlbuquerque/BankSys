615abbf2504c2b843f04a8f802d088e398f4231e
WagnerFrancalino <wagnerfrancalino@hotmail.com>
2016-01-14 14:57:12 -0300

    Closed bug #9 - issues UTF-8

--- test/banksys/persistence/test/AccountVectorTest.java
+++ test/banksys/persistence/test/AccountVectorTest.java
@@ -14,9 +14,9 @@ import banksys.persistence.exception.AccountCreationException;
 public class AccountVectorTest {
 
 	
-	/*Cenário de Teste - Criando vetor de contas com sucesso
+	/*Cenario de Teste - Criando vetor de contas com sucesso
 	 * Dados de Entrada: Conta Ordinary
-	 * Dados de Saída: Vetor de contas criado e populado*/
+	 * Dados de Saida: Vetor de contas criado e populado*/
 	@Test
 	public void testPopulandoVectorContasNormal() throws AccountCreationException{
 		AccountVector accountVector = new AccountVector();
@@ -28,7 +28,7 @@ public class AccountVectorTest {
 		
 		int numberAfter = accountVector.mumberOfAccounts(); 
 		
-		//verificando o tamanho do vector de contas antes e depois da inserção
+		//verificando o tamanho do vector de contas antes e depois da insercao
 		assertFalse(numberAfter == numberBefore);
 	}
 }
