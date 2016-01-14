adaaba67717eb742f4abe6c2cb5cc9d51f5c472d
WagnerFrancalino <wagnerfrancalino@hotmail.com>
2016-01-14 13:57:49 -0300

    Test Case AccountVector - Vector created with Success

---
+++ test/banksys/persistence/test/AccountVectorTest.java
@@ -0,0 +1,35 @@
+package banksys.persistence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import banksys.account.AbstractAccount;
+import banksys.account.OrdinaryAccount;
+import banksys.account.exception.InsufficientFundsException;
+import banksys.account.exception.NegativeAmountException;
+import banksys.persistence.AccountVector;
+import banksys.persistence.exception.AccountCreationException;
+
+public class AccountVectorTest {
+
+	
+	/*Cenário de Teste - Criando vetor de contas com sucesso
+	 * Dados de Entrada: Conta Ordinary
+	 * Dados de Saída: Vetor de contas criado e populado*/
+	@Test
+	public void testPopulandoVectorContasNormal() throws AccountCreationException{
+		AccountVector accountVector = new AccountVector();
+		OrdinaryAccount account = new OrdinaryAccount("1234");
+		
+		int numberBefore = accountVector.mumberOfAccounts(); 
+		
+		accountVector.create(account);
+		
+		int numberAfter = accountVector.mumberOfAccounts(); 
+		
+		//verificando o tamanho do vector de contas antes e depois da inserção
+		assertFalse(numberAfter == numberBefore);
+	}
+}
+
