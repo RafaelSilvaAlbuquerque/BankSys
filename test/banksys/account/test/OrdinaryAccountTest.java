367f2863d2cc9d2069af4fcb4ba3f6d1113ab1c7
francalino <francalinow@gmail.com>
2016-01-18 16:59:01 -0300

    Test account

---
+++ test/banksys/account/test/OrdinaryAccountTest.java
@@ -0,0 +1,14 @@
+package banksys.account.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrdinaryAccountTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
