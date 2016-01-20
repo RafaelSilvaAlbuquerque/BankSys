83536c004101bea0d3a87f222cd8e45f0da31d1e
Priscila-pc <priscila.rfr@gmail.com>
2016-01-19 23:39:38 -0300

    Insert Exception; Create setUp and tearDown; #4

--- test/banksys/account/test/SavingsAccountTest.java
+++ test/banksys/account/test/SavingsAccountTest.java
@@ -2,10 +2,28 @@ package banksys.account.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import banksys.account.SavingsAccount;
+
 
 public class SavingsAccountTest {
 
+	
+private SavingsAccount account;
+	
+	@Before
+	public void setUp() throws Exception {
+	account = new SavingsAccount("12345");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
