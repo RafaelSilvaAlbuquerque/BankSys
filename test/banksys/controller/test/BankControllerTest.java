26bdfbec7a85470f977ad525ff67a79b3e3132af
Priscila-pc <priscila.rfr@gmail.com>
2016-01-20 23:38:32 -0300

    Create class BankControllerTest; #7

---
+++ test/banksys/controller/test/BankControllerTest.java
@@ -0,0 +1,14 @@
+package banksys.controller.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
