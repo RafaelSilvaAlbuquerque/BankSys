e8689cc18f300b9148eb49049c59186c094eab33
wagner.francalino <wagnerfrancalino@hotmail.com>
2016-01-22 14:24:40 -0300

    Test Scenario 2 - Check method getBalance trough ordinaryAccount

--- test/banksys/account/test/AbstractAccountTest.java
+++ test/banksys/account/test/AbstractAccountTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sun.xml.internal.ws.policy.sourcemodel.AssertionData;
+
 import banksys.account.AbstractAccount;
 import banksys.account.OrdinaryAccount;
 import banksys.account.exception.InsufficientFundsException;
@@ -11,12 +13,22 @@ import banksys.account.exception.NegativeAmountException;
 
 public class AbstractAccountTest {
 
-	//Test Scanerio 1 = Check method get number trough class OrdiaryAccount
-	
+	//Test Scenario 1 = Check method get number trough class OrdiaryAccount
 	@Test
-	public void testCheckMehotdGetNumber() {
+	public void testCheckMethodGetNumber() {
 		AbstractAccount ordinayAccount = new OrdinaryAccount("123");
 		assertEquals("123", ordinayAccount.getNumber());
 	}
+	
+	//Test Scenario 2 =  Check method get getBalance trough OrdinaryAccount
+	@Test
+	public void testCheckMethodGetBalance(){
+		AbstractAccount ordinayAccount = new OrdinaryAccount("123");
+		assertEquals(0.0, ordinayAccount.getBalance(),0);
+	}
+
+	
+	
+	
 
 }
