3ab30e5cf80bac83cc40a5972cd4a86972ada7dd
wagner.francalino <wagnerfrancalino@hotmail.com>
2016-01-22 13:00:01 -0300

    Test Scenari 2: Debit amount < 0

--- test/banksys/account/test/OrdinaryAccountTest.java
+++ test/banksys/account/test/OrdinaryAccountTest.java
@@ -1,4 +1,5 @@
 package banksys.account.test;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,18 +8,23 @@ import banksys.account.OrdinaryAccount;
 import banksys.account.exception.InsufficientFundsException;
 import banksys.account.exception.NegativeAmountException;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class OrdinaryAccountTest {
 	// Test Scenario 1: Debit of account in balance Empty
-	@Test (expected=InsufficientFundsException.class)
+	@Test(expected = InsufficientFundsException.class)
 	public void testDebitAccountBalanceEmpty() throws NegativeAmountException, InsufficientFundsException {
 		OrdinaryAccount ordinayAccount = new OrdinaryAccount("1234");
-		ordinayAccount.debit(12);	
+		ordinayAccount.debit(12);
 	}
-}
-	
 
+	// Test Scenari 2: Debit amount < 0
+	@Test(expected = NegativeAmountException.class)
+	public void testDebitAccountAmountSmaller() throws NegativeAmountException, InsufficientFundsException {
+		OrdinaryAccount ordinayAccount = new OrdinaryAccount("1234");
+		ordinayAccount.debit(-12);
+	}
+
+}
