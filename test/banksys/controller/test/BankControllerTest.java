2fab256f14c284acbab8dede3edf24bd643a6109
Priscila-pc <priscila.rfr@gmail.com>
2016-01-23 23:51:39 -0300

    @Before and @After BankController Test; Scenario Test 1; #7

--- test/banksys/controller/test/BankControllerTest.java
+++ test/banksys/controller/test/BankControllerTest.java
@@ -2,10 +2,26 @@ package banksys.controller.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import banksys.account.AbstractAccount;
+import banksys.account.SavingsAccount;
 
 public class BankControllerTest {
 
+private AbstractAccount account;
+	
+	@Before
+	public void setUp() throws Exception {
+	account = new SavingsAccount("12345");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
