f52a65fcc9cf74032f393451bf05a278919fbfac
WagnerFrancalino <wagnerfrancalino@hotmail.com>
2016-01-25 09:59:57 -0300

    Test Scenario 1 - Check method get number trough class OrdiaryAccount after error

--- test/banksys/account/test/AbstractAccountTest.java
+++ test/banksys/account/test/AbstractAccountTest.java
@@ -1,5 +1,17 @@
 package banksys.account.test;
 
-public class AbstractAccountTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import banksys.account.AbstractAccount;
+import banksys.account.OrdinaryAccount;
+
+public class AbstractAccountTest {
+	//Test Scenario 1 - Check method get number trough class OrdiaryAccount
+		@Test
+		public void testCheckMethodGetNumber() {
+			AbstractAccount ordinayAccount = new OrdinaryAccount("123");
+			assertEquals("123", ordinayAccount.getNumber());
+		}
 }
