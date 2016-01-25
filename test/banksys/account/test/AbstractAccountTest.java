269eabd99b85a58494c4f8e313e5e0969873b760
WagnerFrancalino <wagnerfrancalino@hotmail.com>
2016-01-25 10:17:45 -0300

    Test Scenario 3 - Check method credit in amount negative

--- test/banksys/account/test/AbstractAccountTest.java
+++ test/banksys/account/test/AbstractAccountTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import banksys.account.AbstractAccount;
 import banksys.account.OrdinaryAccount;
+import banksys.account.exception.NegativeAmountException;
 
 public class AbstractAccountTest {
 	    //Test Scenario 1 - Check method get number trough class OrdiaryAccount
@@ -20,5 +21,12 @@ public class AbstractAccountTest {
 		public void testCheckMethodGetBalance(){
 			AbstractAccount ordinayAccount = new OrdinaryAccount("123");
 			assertTrue(0.0 == ordinayAccount.getBalance());
+		}
+		
+		//Test Scenario 3 - Check method credit in amount negative
+		@Test(expected=NegativeAmountException.class)
+		public void testMethodCreditAmountNegative() throws NegativeAmountException{
+			AbstractAccount ordinayAccount = new OrdinaryAccount("123");
+			ordinayAccount.credit(-2.0);
 		}
 }
