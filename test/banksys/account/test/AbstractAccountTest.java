321f5571fe641508360aa0a251f413b449bc9769
WagnerFrancalino <wagnerfrancalino@hotmail.com>
2016-01-25 10:03:17 -0300

    Test Scenario 2 - Check method getBalance trough OrdinaryAccount after error

--- test/banksys/account/test/AbstractAccountTest.java
+++ test/banksys/account/test/AbstractAccountTest.java
@@ -8,10 +8,17 @@ import banksys.account.AbstractAccount;
 import banksys.account.OrdinaryAccount;
 
 public class AbstractAccountTest {
-	//Test Scenario 1 - Check method get number trough class OrdiaryAccount
+	    //Test Scenario 1 - Check method get number trough class OrdiaryAccount
 		@Test
 		public void testCheckMethodGetNumber() {
 			AbstractAccount ordinayAccount = new OrdinaryAccount("123");
 			assertEquals("123", ordinayAccount.getNumber());
+		}
+		
+		//Test Scenario 2 - Check method getBalance trough OrdinaryAccount
+		@Test
+		public void testCheckMethodGetBalance(){
+			AbstractAccount ordinayAccount = new OrdinaryAccount("123");
+			assertTrue(0.0 == ordinayAccount.getBalance());
 		}
 }
